809262343ed597126e1198ffdce15e8789059bc8
selinaXyy <158121120+selinaXyy@users.noreply.github.com>
2024-03-16 14:42:02 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:thc/models/bloc.dart';
-import 'package:thc/models/local_storage.dart';
-import 'package:thc/models/navigator.dart';
-import 'package:thc/models/theme.dart';
-import 'package:thc/models/user.dart';
-import 'package:thc/views/home/home_screen.dart';
-import 'package:thc/views/settings/settings.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await loadFromLocalStorage();
-  runApp(const App());
+void main() {
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        BlocProvider(create: (_) => AppTheme()),
-      ],
-      builder: (context, _) => MaterialApp(
-        navigatorKey: navKey,
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: context.watch<AppTheme>().state,
-        debugShowCheckedModeBanner: false,
-        home: const ChooseAnyView(),
-      ),
-    );
-  }
-}
-
-/// {@template main.ChooseAnyView}
-/// Change the value of `home` to anything you want!
-///
-/// If you're working on the login screen, you can use
-/// ```dart
-///   home: const LoginScreen(),
-/// ```
-/// {@endtemplate}
-class ChooseAnyView extends StatelessWidget {
-  /// {@macro main.ChooseAnyView}
-  const ChooseAnyView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(flex: 3),
-            Image.asset('assets/thc_logo_with_text.png', width: 250),
-            const Spacer(flex: 2),
-            for (final type in UserType.values) UserButton(type),
-            const Spacer(flex: 3),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+          actions: [
+            IconButton(onPressed: (){
+              //handle logout press
+            }, icon: const Icon(Icons.logout))
           ],
+          backgroundColor: const Color.fromARGB(255, 131, 124, 234),
         ),
-      ),
-    );
-  }
-}
-
-class UserButton extends StatelessWidget {
-  const UserButton(this.type, {super.key});
-  final UserType type;
-
-  @override
-  Widget build(BuildContext context) {
-    final colors = context.colorScheme;
-    final buttonStyle = switch (type) {
-      UserType.participant => ElevatedButton.styleFrom(
-          backgroundColor: colors.primary,
-          foregroundColor: colors.onPrimary,
-        ),
-      UserType.director => ElevatedButton.styleFrom(
-          backgroundColor: colors.secondary,
-          foregroundColor: colors.onSecondary,
-        ),
-      UserType.admin => ElevatedButton.styleFrom(
-          backgroundColor: colors.surface,
-          foregroundColor: colors.onSurface,
-        ),
-    };
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      child: ElevatedButton(
-        onPressed: () {
-          userType = type;
-          navigator.pushReplacement(const HomeScreen());
-        },
-        style: buttonStyle,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Text('view as $type', style: const TextStyle(fontSize: 18)),
+        body: const Center(child: Text("Implementation of body content")),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+            BottomNavigationBarItem(label: "About Us", icon: Icon(Icons.info))
+          ],
         ),
       ),
     );
