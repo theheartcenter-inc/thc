0e738364f701c940a18b9a90c283972bd51c09b7
Nate Wilson <nate.w5687@gmail.com>
2024-03-16 14:17:20 -0600

    add import from submodule into agora.dart

--- lib/models/agora/agora.dart
+++ lib/models/agora/agora.dart
@@ -1,1 +1,13 @@
+/// This file imports credentials from a git submodule to configure our Agora settings.
+///
+/// By using a private submodule, we can be a part of the
+/// [open source initiative](https://opensource.org/) and still maintain security!
+library;
 
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:thc/models/credentials/credentials.dart';
+
+const engineContext = RtcEngineContext(
+  appId: AgoraCredentials.id,
+  channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+);
