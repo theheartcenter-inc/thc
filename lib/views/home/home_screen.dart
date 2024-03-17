6927b75172dbe742b515eab09476d2bb52b7128f
Selina Xue <selinaxue@selinas.home>
2024-03-16 18:12:08 -0700

    run auto-formatter

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -22,22 +22,25 @@ class ParticipantHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Home"),
-          actions: [
-            IconButton(onPressed: (){
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () {
               //handle logout press
-            }, icon: const Icon(Icons.logout))
-          ],
-          backgroundColor: const Color.fromARGB(255, 131, 124, 234),
-        ),
-        body: const Center(child: Text("Implementation of body content")),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-            BottomNavigationBarItem(label: "About Us", icon: Icon(Icons.info))
-          ],
-        ),
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
+        backgroundColor: const Color.fromARGB(255, 131, 124, 234),
+      ),
+      body: const Center(child: Text("Implementation of body content")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "About Us", icon: Icon(Icons.info))
+        ],
+      ),
     );
   }
 }
