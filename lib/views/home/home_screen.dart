c7882c69638cce944b46a2950a4fd88da4097353
Nate Wilson <nate.w5687@gmail.com>
2024-03-17 20:24:30 -0600

    undo autoformat

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -29,45 +29,43 @@ class _ParticipantHomeScreenState extends State<ParticipantHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_currentIndex == 0 ? 'Home' : 'About Us'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              //await handle logout press
-            },
-            icon: const Icon(Icons.logout),
-          )
-        ],
-        backgroundColor: _currentIndex == 0
-            ? const Color.fromARGB(255, 131, 124, 234)
-            : const Color.fromARGB(199, 153, 205, 154),
-      ),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: const [
-          //home page (index 0)
-          Center(
-            child: Text('Homepage Implementation'),
-          ),
-          //about us (index 1)
-          Center(
-            child: Text('About Us Implementation'),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: 'About Us', icon: Icon(Icons.info)),
-        ],
-      ),
+        appBar: AppBar(
+          title: Text(_currentIndex == 0 ? 'Home' : 'About Us'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                //await handle logout press
+              },
+              icon: const Icon(Icons.logout),
+            )
+          ],
+          backgroundColor: _currentIndex == 0 ? const Color.fromARGB(255, 131, 124, 234) : const Color.fromARGB(199, 153, 205, 154),
+        ),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: const [
+            //home page (index 0)
+            Center(
+              child: Text('Homepage Implementation'),
+            ),
+            //about us (index 1)
+            Center(
+              child: Text('About Us Implementation'),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+            BottomNavigationBarItem(label: 'About Us', icon: Icon(Icons.info)),
+          ],
+        ),
     );
   }
 }
