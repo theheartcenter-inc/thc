e64a5e4b13f73e3dea1f6a13b052fc51230ab012
Nate Wilson <nate.w5687@gmail.com>
2024-03-17 20:46:40 -0600

    copy over @selinaXyy's home screen changes

--- lib/views/home/participant_home.dart
+++ lib/views/home/participant_home.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:thc/models/theme.dart';
 
-class ParticipantHomeScreen extends StatelessWidget {
+class ParticipantHomeScreen extends StatefulWidget {
   const ParticipantHomeScreen({super.key});
+
+  @override
+  State<ParticipantHomeScreen> createState() => _ParticipantHomeScreenState();
+}
+
+class _ParticipantHomeScreenState extends State<ParticipantHomeScreen> {
+  int _currentIndex = 0; // default
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Text(_currentIndex == 0 ? 'Home' : 'About Us'),
         actions: [
           IconButton(
-            onPressed: () {
-              // handle logout press
+            onPressed: () async {
+              // await handle logout press
             },
             icon: const Icon(Icons.logout),
           )
         ],
-        backgroundColor: const Color.fromARGB(255, 131, 124, 234),
+        backgroundColor: _currentIndex == 0 ? ThcColors.darkMagenta : ThcColors.green,
       ),
-      body: const Center(child: Text('Implementation of body content')),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          // home page (index 0)
+          Center(
+            child: Text('Homepage Implementation'),
+          ),
+          // about us (index 1)
+          Center(
+            child: Text('About Us Implementation'),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: 'About Us', icon: Icon(Icons.info))
+          BottomNavigationBarItem(label: 'About Us', icon: Icon(Icons.info)),
         ],
       ),
     );
