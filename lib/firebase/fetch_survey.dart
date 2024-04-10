1a437ab2ad8d1035363bcd9da53fe7dd97597b16
Nate Wilson <nate.w5687@gmail.com>
2024-04-10 09:25:26 -0600

    fix analysis problems

--- lib/firebase/fetch_survey.dart
+++ lib/firebase/fetch_survey.dart
@@ -1,52 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:thc/firebase/firebase.dart';
 
 // Data model for a survey question
-class SurveyQuestion {
-  final String id;
-  final String text;
+class SurveySnapshot {
+  SurveySnapshot({required this.id, required this.text});
 
-  SurveyQuestion({required this.id, required this.text});
-
-  factory SurveyQuestion.fromSnapshot(DocumentSnapshot snapshot) {
-    return SurveyQuestion(
+  factory SurveySnapshot.fromSnapshot(DocumentSnapshot snapshot) {
+    return SurveySnapshot(
       id: snapshot.id,
       text: snapshot['text'],
     );
   }
+
+  final String id;
+  final String text;
 }
 
 // Firebase service to interact with Firestore
-class FirebaseService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // Fetches survey questions for a given participant
-  Stream<List<SurveyQuestion>> getSurveyQuestionsForParticipant(
-      String participantId) {
-    return _firestore
-        .collection('users')
-        .doc(participantId)
-        .collection('survey_questions')
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs
-          .map((doc) => SurveyQuestion.fromSnapshot(doc))
-          .toList();
-    });
-  }
-}
-
-void main() {
-  FirebaseService firebaseService = FirebaseService();
-  String participantId =
-      'test_participant'; 
-
-  firebaseService
-      .getSurveyQuestionsForParticipant(participantId)
-      .listen((questions) {
-    print(
-        'Received ${questions.length} survey questions for participant $participantId:');
-    for (var question in questions) {
-      print('${question.id}: ${question.text}');
-    }
+Stream<List<SurveySnapshot>> getSurveyQuestionsForParticipant(String participantId) {
+  return db
+      .collection('users')
+      .doc(participantId)
+      .collection('survey_questions')
+      .snapshots()
+      .map((snapshot) {
+    return snapshot.docs.map(SurveySnapshot.fromSnapshot).toList();
   });
 }
