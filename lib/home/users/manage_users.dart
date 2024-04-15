30b9db6010e588904d18a8d1b222fc33f6930247
Nate Wilson <nate.w5687@gmail.com>
2024-04-15 16:58:23 -0600

    `ManageUsers` fixes

--- lib/home/users/manage_users.dart
+++ lib/home/users/manage_users.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:thc/firebase/firebase.dart';
+import 'package:thc/utils/app_config.dart';
 
 class ManageUsers extends StatefulWidget {
   const ManageUsers({super.key});
@@ -9,60 +11,55 @@ class ManageUsers extends StatefulWidget {
 }
 
 class _ManageUsersState extends State<ManageUsers> {
+  final horizontalScroll = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    horizontalScroll.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Management'),
+    final dataTable = StreamBuilder<QuerySnapshot>(
+      stream: db.collection('users').snapshots(),
+      builder: (context, snapshot) => DataTable(
+        sortColumnIndex: 0,
+        columns: const [
+          DataColumn(label: Text('Id')),
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Type')),
+          DataColumn(label: Text('Actions'))
+        ],
+        rows: [
+          if (snapshot.data?.docs.reversed case final users?)
+            for (final user in users)
+              DataRow(cells: [
+                DataCell(Text(user['id'])),
+                DataCell(Text(user['name'])),
+                DataCell(Text(user['type'])),
+                const DataCell(Text(''), placeholder: true),
+              ]),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('users').snapshots(),
-          builder: (context, snapshot) {
-            final List<DataRow> userWidgets = [];
-            if (snapshot.hasData) {
-              final users = snapshot.data?.docs.reversed.toList();
-              for (var user in users!) {
-                final userWidget = DataRow(selected: true, cells: [
-                  DataCell(Text(user['id'])),
-                  DataCell(Text(user['name'])),
-                  DataCell(Text(user['type'])),
-                  const DataCell(Text(''), placeholder: true),
-                ]);
-                userWidgets.add(userWidget);
-              }
-            }
-            return Expanded(
-              child: Scrollbar(
-                child: ListView(
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: SingleChildScrollView(
-                        child: DataTable(
-                          // headingTextStyle: const TextStyle(
-                          //   fontSize: 14,
-                          // ),
-                          // dataTextStyle: const TextStyle(
-                          //   fontSize: 12,
-                          // ),
-                          sortColumnIndex: 0,
-                          columns: const [
-                            DataColumn(label: Text('Id')),
-                            DataColumn(label: Text('Name')),
-                            DataColumn(label: Text('Type')),
-                            DataColumn(label: Text('Actions'))
-                          ],
-                          rows: userWidgets,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+    );
+
+    return LayoutBuilder(
+      builder: (context, constraints) => SingleChildScrollView(
+        child: Scrollbar(
+          controller: horizontalScroll,
+          thumbVisibility: mobileDevice ? null : true,
+          child: SingleChildScrollView(
+            controller: horizontalScroll,
+            scrollDirection: Axis.horizontal,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minWidth: constraints.maxWidth),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: dataTable,
               ),
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
