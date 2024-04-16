4057d66d6bf6d0647a77ebb6a5e21dfcf5f751a9
Nate Wilson <nate.w5687@gmail.com>
2024-04-16 11:25:44 -0600

    revert unrelated change

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 /// Use `if (mobileDevice)` when you want different behavior
 /// based on whether a touchscreen or mouse cursor is being used.
-final mobileDevice = switch (defaultTargetPlatform) {
-  TargetPlatform.android || TargetPlatform.iOS => true,
-  _ => false,
-};
+final mobileDevice = switch (Platform.operatingSystem) { 'ios' || 'android' => true, _ => false };
 
 /// Set this to `false` if you're just working on frontend stuff
 /// and you don't want to worry about connecting with Agora or Firebase.
