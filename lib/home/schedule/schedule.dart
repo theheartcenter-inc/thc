1184746b3a709c6df8d7bb22ce751d4122b9c6a9
ChaAbby <68627454+ChaAbby@users.noreply.github.com>
2024-05-14 11:45:20 -0700

    Remove package
    Co-authored-by: Nate <nate.w5687@gmail.com>

--- lib/home/schedule/schedule.dart
+++ lib/home/schedule/schedule.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:thc/firebase/firebase.dart';
