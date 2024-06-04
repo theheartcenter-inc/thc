cd77202dafa2d050993b714149a76850314c63c3
BooooobZhao <xuebao.zhao1999@gmail.com>
2024-06-04 05:45:48 -0700

    add/remove-videos

--- lib/home/library/video_library.dart
+++ lib/home/library/video_library.dart
@@ -10,6 +10,9 @@ import 'package:thc/utils/theme.dart';
 import 'package:intl/intl.dart';
 import 'package:dio/dio.dart';
 import 'package:universal_html/html.dart' as html;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:thc/utils/widgets/error_dialog.dart';
+import 'package:file_picker/file_picker.dart';
 
 class FavoriteIconButton extends StatelessWidget {
   const FavoriteIconButton({
@@ -42,6 +45,8 @@ class VideoCard extends StatelessWidget {
     this.thumbnail,
     this.isPinned = false,
     required this.onFavoriteToggle,
+    required this.isAdmin,
+    required this.onDelete,
   });
 
   final String title;
@@ -53,6 +58,8 @@ class VideoCard extends StatelessWidget {
   final String? thumbnail;
   final bool isPinned;
   final VoidCallback onFavoriteToggle;
+  final bool isAdmin;
+  final VoidCallback onDelete;
 
   Future<String?> playVideo() async {
     if (!kIsWeb && defaultTargetPlatform == TargetPlatform.windows) {
@@ -117,6 +124,51 @@ class VideoCard extends StatelessWidget {
     }
   }
 
+  void confirmDelete(BuildContext context, String videoId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this video?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('Confirm'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                deleteVideo(context, videoId); // Proceed with deleting the video
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Add a method to delete the video
+  Future<void> deleteVideo(BuildContext context, String videoId) async {
+    try {
+      final doc = FirebaseFirestore.instance.collection('streams').doc(videoId);
+      final docSnapshot = await doc.get();
+      if (docSnapshot.exists) {
+        final filePath = docSnapshot.data()?['storage_path'];
+        final ref = FirebaseStorage.instance.ref().child(filePath);
+        await ref.delete();
+        await doc.delete();
+        onDelete();
+      }
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Video successfully deleted!')));
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Failed to delete video!')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final Image image;
@@ -154,8 +206,6 @@ class VideoCard extends StatelessWidget {
         },
         hoverColor: ThcColors.dullBlue.withOpacity(1 / 8),
         leading: image,
-        // Add a trailing favorite icon button
-        // Add a download button
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -169,6 +219,11 @@ class VideoCard extends StatelessWidget {
               icon: const Icon(Icons.download),
               onPressed: downloadVideo,
             ),
+            if (isAdmin)
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => confirmDelete(context, id),
+              ),
           ],
         ),
       ),
@@ -189,11 +244,38 @@ class _VideoLibraryState extends State<VideoLibrary> {
   List<VideoCard> allVideos = [];
   List<VideoCard> videos = [];
   Set<String> ids = {};
+  String? userEmail;
+  bool isAdmin = false; // This should be fetched based on the logged-in user's role
 
   @override
   void initState() {
     super.initState();
+    getCurrentUserEmail();
     fetchDocuments();
+  }
+
+  void getCurrentUserEmail() {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      userEmail = user.email; // Get the user's email
+      checkAdminRole(); // Check if the user is an admin
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const ErrorDialog('Login Required!');
+        },
+      );
+    }
+  }
+
+  // Fetching isAdmin status from Firestore
+  void checkAdminRole() async {
+    if (userEmail == null) return; // Ensure the userId is not null
+    final userDoc = await FirebaseFirestore.instance.collection('users').doc(userEmail).get();
+    setState(() {
+      isAdmin = userDoc.data()?['type'] == 'Admin';
+    });
   }
 
   Future<void> fetchDocuments() async {
@@ -215,9 +297,20 @@ class _VideoLibraryState extends State<VideoLibrary> {
             timestamp: document['date'],
             isPinned: pinnedIds.contains(document.id),
             onFavoriteToggle: () => toggleFavorite(document.id),
+            isAdmin: isAdmin,
+            onDelete: () => onDelete(document.id),
           ),
       ];
     });
+  }
+
+  void onDelete(String videoId) {
+    setState(() {
+      // Remove the deleted video from the local list of videos
+      allVideos.removeWhere((video) => video.id == videoId);
+      videos.removeWhere((video) => video.id == videoId);
+    });
+    fetchDocuments();
   }
 
   void toggleFavorite(String videoId) async {
@@ -265,6 +358,144 @@ class _VideoLibraryState extends State<VideoLibrary> {
     });
   }
 
+  Future selectFile() async {
+    final FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    final Uint8List? fileBytes = result.files.first.bytes;
+    final String fileName = result.files.first.name;
+    try {
+      final ref = FirebaseStorage.instance.ref(fileName);
+      final uploadTask = ref.putData(fileBytes!);
+      await uploadTask.whenComplete(() {
+        if (uploadTask.snapshot.state == TaskState.success) {
+          Navigator.of(context).pop();
+          uploadDetailsDialog(context, fileName);
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return const ErrorDialog('File upload failed.');
+            },
+          );
+        }
+      });
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const ErrorDialog('Error uploading file.');
+        },
+      );
+    }
+  }
+
+  Future<void> uploadDetailsDialog(BuildContext context, String path) async {
+    String category = '';
+    String director = '';
+    String title = '';
+    final Timestamp date = Timestamp.now();
+    path = 'gs://the-heart-center-app.appspot.com/$path';
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Upload Details'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Category'),
+                onChanged: (value) {
+                  category = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Director'),
+                onChanged: (value) {
+                  director = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                onChanged: (value) {
+                  title = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                fetchDocuments();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final newStreams = FirebaseFirestore.instance.collection('streams').doc();
+                newStreams.set(
+                  {
+                    'category': category,
+                    'date': date,
+                    'director': director,
+                    'id': newStreams.id,
+                    'title': title,
+                    'storage_path': path,
+                  },
+                );
+                Navigator.of(context).pop(); // Close the dialog
+                setState(() {
+                  allVideos.add(
+                    VideoCard(
+                      id: newStreams.id,
+                      title: title,
+                      category: category,
+                      director: director,
+                      path: path,
+                      timestamp: date,
+                      onFavoriteToggle: () => toggleFavorite(newStreams.id),
+                      isAdmin: isAdmin,
+                      onDelete: () => onDelete(newStreams.id),
+                    ),
+                  );
+                  videos = allVideos;
+                });
+              },
+              child: const Text('Upload'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void selectUploadFileDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Upload your file'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              onPressed: selectFile,
+              child: const Text('Upload File'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> handleFileUpload() async {
+    selectUploadFileDialog(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     if (allVideos.isEmpty) {
@@ -291,43 +522,55 @@ class _VideoLibraryState extends State<VideoLibrary> {
       ),
     );
 
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-          child: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.search),
-              hintText: 'Search here',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Library'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Search here',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
+              onChanged: searchVideo,
             ),
-            onChanged: searchVideo,
           ),
-        ),
-        DropdownButton<String>(
-          value: selectedCategory,
-          onChanged: (newValue) {
-            setState(() {
-              selectedCategory = newValue!;
-              filterVideos();
-            });
-          },
-          items: [
-            for (final category in categories)
-              DropdownMenuItem<String>(value: category, child: Text(category)),
-          ],
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: videos.length,
-            itemBuilder: (context, index) => videos[index],
-            prototypeItem: blankCard,
+          DropdownButton<String>(
+            value: selectedCategory,
+            onChanged: (newValue) {
+              setState(() {
+                selectedCategory = newValue!;
+                filterVideos();
+              });
+            },
+            items: [
+              for (final category in categories)
+                DropdownMenuItem<String>(value: category, child: Text(category)),
+            ],
           ),
-        ),
-      ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: videos.length,
+              itemBuilder: (context, index) => videos[index],
+              prototypeItem: blankCard,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: isAdmin
+          ? FloatingActionButton(
+              backgroundColor: Colors.green,
+              onPressed: handleFileUpload,
+              child: const Icon(Icons.add),
+            )
+          : null,
     );
   }
 }
