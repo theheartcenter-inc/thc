632c5a6a4d0d942510128eea0cd9c4a81a9e850c
Nate Wilson <nate.w5687@gmail.com>
2024-08-27 20:12:05 -0600

    VERY IMPORTANT CHANGE

--- lib/home/profile/settings/settings.dart
+++ lib/home/profile/settings/settings.dart
@@ -8,7 +8,6 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
       body: Center(
