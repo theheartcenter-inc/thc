623085088fa2bad4d002139860fd0bff4ad7c20c
Nate Wilson <nate.w5687@gmail.com>
2024-09-28 09:47:53 -0600

    cleanup!

--- lib/agora/channel/create_channel.dart
+++ lib/agora/channel/create_channel.dart
@@ -1,7 +1,6 @@
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:flutter/material.dart';
-import 'package:thc/home/schedule/scheduled_stream_card.dart';
+import 'package:thc/firebase/firebase_auth.dart' as auth;
 import 'package:thc/home/schedule/src/all_scheduled_streams.dart';
+import 'package:thc/the_good_stuff.dart';
 import 'pre_joining_dialog.dart';
 
 class CreateChannelPage extends StatefulWidget {
@@ -12,243 +11,140 @@ class CreateChannelPage extends StatefulWidget {
 }
 
 class _CreateChannelPageState extends State<CreateChannelPage> {
-  final _formKey = GlobalKey<FormState>();
+  final _formKey = FormKey();
 
-  late final FocusNode _unfocusNode;
-  late final TextEditingController _channelNameController;
+  final _unfocusNode = FocusNode();
+  final _channelNameController = TextEditingController();
 
   bool _isCreatingChannel = false;
 
   @override
-  void initState() {
-    super.initState();
-    _unfocusNode = FocusNode();
-    _channelNameController = TextEditingController();
-  }
-
-  @override
   void dispose() {
     _unfocusNode.dispose();
+    _channelNameController.dispose();
     super.dispose();
   }
 
-  void _showSnackBar(BuildContext context, String text) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(text),
-      ),
-    );
-  }
-
-  String? _channelNameValidator(value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter a channel name';
-    } else if (value.length > 64) {
-      return 'Channel name must be less than 64 characters';
-    }
-    return null;
-  }
+  String? _channelNameValidator(String? value) => switch (value) {
+        null || '' => 'Please enter a channel name',
+        String(length: > 64) => 'Channel name must be less than 64 characters',
+        _ => null,
+      };
 
   Future<void> _joinRoom() async {
-    if (!_formKey.currentState!.validate()) {
+    if (!_formKey.validate()) {
       return;
     }
     FocusScope.of(context).requestFocus(_unfocusNode);
     setState(() => _isCreatingChannel = true);
-    final input = <String, dynamic>{
-      'channelName': _channelNameController.text,
-      'expiryTime': 3600, // 1 hour
-    };
     try {
-      final response =
-          await FirebaseFunctions.instance.httpsCallable('generateToken').call(input);
-      final token = response.data as String?;
-      if (token != null) {
-        if (context.mounted) {
-          _showSnackBar(
-            context,
-            'Token generated successfully!',
-          );
-        }
-        await Future.delayed(
-          const Duration(seconds: 1),
+      final response = await auth.generateToken(<String, dynamic>{
+        'channelName': _channelNameController.text,
+        'expiryTime': 3600, // 1 hour
+      });
+      if (response.data case final token?) {
+        navigator.snackbarMessage('Token generated successfully!');
+        await Future.delayed(const Duration(seconds: 1));
+        await navigator.showDialog(
+          PreJoiningDialog(
+            channelName: _channelNameController.text,
+            token: token,
+          ),
         );
-        if (context.mounted) {
-          await showDialog(
-            context: context,
-            builder: (context) => PreJoiningDialog(
-              channelName: _channelNameController.text,
-              token: token,
-            ),
-          );
-        }
       }
     } catch (e) {
-      _showSnackBar(
-        context,
-        'Error generating token: $e',
-      );
-    } finally {
-      setState(() => _isCreatingChannel = false);
+      navigator.snackbarMessage('Error generating token: $e');
     }
-  }
-
-  getScheduledStreams(user) {
-    final (:active, :scheduled) = ScheduledStreams.of(context);
-    var stream;
-    var title;
-    final List<Widget> userStreams = [];
-    for (stream in scheduled) {
-      if (stream.director == user) {
-        title = stream.title;
-        var scheduledStream = stream;
-        userStreams.add(GestureDetector(
-          child: scheduledStream,
-          onTap: () => updateNameController(scheduledStream.title)),
-        );
-      }
-    }
-    return userStreams;
-  }
-
-  Future updateNameController(title) async {
-    _channelNameController.text = title;
+    setState(() => _isCreatingChannel = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          leading: const BackButton(),
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
+    final mainContent = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 8.0),
+          child: Text(
+            'Select or Create Channel',
+            style: TextStyle(size: 32.0, weight: 500),
+          ),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: screenSize.width,
-              constraints: const BoxConstraints(maxWidth: 600),
-              child: Padding(
-                padding: const EdgeInsetsDirectional.symmetric(horizontal: 24.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(
-                          0.0,
-                          30.0,
-                          0.0,
-                          8.0,
-                        ),
-                        child: Text(
-                          'Select or Create Channel',
-                          style: TextStyle(
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsetsDirectional.only(bottom: 24.0),
-                        child: Column(
-                          children: getScheduledStreams('Liz Lemon'),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsetsDirectional.only(bottom: 24.0),
-                        child: Text(
-                          'Enter a channel name to generate token. The token will be valid for 1 hour.',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          autofocus: true,
-                          controller: _channelNameController,
-                          decoration: InputDecoration(
-                            labelText: 'Channel Name',
-                            labelStyle: const TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.normal,
-                            ),
-                            hintText: 'Enter your channel name...',
-                            hintStyle: const TextStyle(
-                              color: Color(0xFF57636C),
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.normal,
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                                width: 2.0,
-                              ),
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            errorBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.red,
-                              ),
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            focusedErrorBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.red,
-                                width: 2.0,
-                              ),
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                          ),
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.normal,
-                          ),
-                          keyboardType: TextInputType.text,
-                          validator: _channelNameValidator,
-                        ),
-                      ),
-                      const SizedBox(height: 24.0),
-                      if (_isCreatingChannel)
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [CircularProgressIndicator()],
-                        )
-                      else
-                        SizedBox(
-                          width: double.infinity,
-                          height: 55,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                            ),
-                            onPressed: _joinRoom,
-                            child: const Text('Join Room'),
-                          ),
-                        ),
-                    ],
+        Padding(
+          padding: const EdgeInsetsDirectional.only(bottom: 24.0),
+          child: Column(
+            children: [
+              for (final scheduledStream in ScheduledStreams.of(context).scheduled)
+                if (scheduledStream.director == 'Liz Lemon')
+                  GestureDetector(
+                    child: scheduledStream,
+                    onTap: () => _channelNameController.text = scheduledStream.title,
                   ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsetsDirectional.only(bottom: 24.0),
+          child: Text(
+            'Enter a channel name to generate token. The token will be valid for 1 hour.',
+            style: TextStyle(color: Colors.grey, size: 16.0),
+          ),
+        ),
+        Form(
+          key: _formKey,
+          child: TextFormField(
+            autofocus: true,
+            controller: _channelNameController,
+            decoration: InputDecoration(
+              labelText: 'Channel Name',
+              labelStyle: const TextStyle(color: Colors.blue, size: 16.0),
+              hintText: 'Enter your channel name...',
+              hintStyle: const TextStyle(color: Color(0xFF57636C), size: 16.0),
+              border: MaterialStateOutlineInputBorder.resolveWith(
+                (states) => OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: states.isError ? Colors.red : Colors.blue,
+                    width: states.isFocused ? 2.0 : 1.0,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(12.0)),
                 ),
               ),
             ),
-          ],
+            style: const TextStyle(color: Colors.black, size: 16.0),
+            keyboardType: TextInputType.text,
+            validator: _channelNameValidator,
+          ),
+        ),
+        const SizedBox(height: 24.0),
+        if (_isCreatingChannel)
+          const Center(heightFactor: 1.0, child: CircularProgressIndicator())
+        else
+          SizedBox(
+            width: double.infinity,
+            height: 55,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              onPressed: _joinRoom,
+              child: const Text('Join Room'),
+            ),
+          ),
+      ],
+    );
+
+    return GestureDetector(
+      onTap: _unfocusNode.requestFocus,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(backgroundColor: Colors.white, surfaceTintColor: Colors.white),
+        body: Center(
+          heightFactor: 1.0,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              width: 600,
+              child: SingleChildScrollView(child: mainContent),
+            ),
+          ),
         ),
       ),
     );
